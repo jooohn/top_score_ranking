70de238ef9cd290adf76e0c38eed2303d9b14bf9
Jun Tomioka <jooohn12341234@gmail.com>
2021-07-31 17:09:14 +0900

    Add db constraints and index

--- db/migrate/20210729144854_create_scores.rb
+++ db/migrate/20210729144854_create_scores.rb
@@ -1,9 +1,11 @@
 class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
-      t.string :player
-      t.integer :score
-      t.datetime :time
+      t.string :player, null: false
+      t.integer :score, null: false
+      t.datetime :time, null: false
     end
+    add_index :scores, [:player, :time]
+    add_index :scores, [:time]
   end
 end
